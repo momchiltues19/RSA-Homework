55700504e972b29c21b9b3eeea869a7e4cf15a30
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-12-12 18:10:40 +0200

    Update RSA.rb

--- lib/RSA.rb
+++ lib/RSA.rb
@@ -1,1 +1,33 @@
-init
+class RSA
+   def initialize n, e, d
+      @n = n
+      @e = e
+      @d = d
+   end
+   def n
+      @n
+   end
+   
+   def e
+      @e
+   end
+   
+   def d
+      @d
+   end
+   
+   def new_key
+      #generates a new 'n','e' and 'd' values following the RSA algorithm. Returns a new array of three elements where the first element is 'n', the second is 'e' and the third is 'd'. Each time it is called a new key must be returned.
+   end
+   
+   def encrypt message
+      #encrypts the message passed. The message is of type string. Encrypts each symbol of this string by using its ASCII number representation and returns the encrypted message.
+   end
+   
+   def decrypt message
+      #decrypts the message passed. The message is of type string. Decrypts each symbol of this string Encrypts each symbol of this string by using its ASCII number representationand returns the decrypted message. 
+   end 
+end
+
+test = RSA.new
+print test.n
