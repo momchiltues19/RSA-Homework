d31539a3cc068e91b8f882010b642f95a14ba993
Momchil Todorov <mom.todorov.tues19@gmail.com>
2017-12-13 00:38:24 +0200

    whitespace_fix

--- spec/RSA_spec.rb
+++ spec/RSA_spec.rb
@@ -2,18 +2,18 @@ require "RSA"
 
 RSpec.describe RSA do
 	describe "when trying to use getters " do
-			rsa = RSA.new(1,2,3)
-			it "checks if n is equal to 1" do
-				expect(rsa.n).to eq 1
-			end
-			it "checks if e is equal to 2" do
-				expect(rsa.e).to eq 2 
-			end
-			it "checks if d is equal to 3" do
-				expect(rsa.d).to eq 3 
-			end
-  end
-  describe "when trying to encrypt/decrypt a message "
+		rsa = RSA.new(1,2,3)
+		it "checks if n is equal to 1" do
+			expect(rsa.n).to eq 1
+		end
+		it "checks if e is equal to 2" do
+			expect(rsa.e).to eq 2 
+		end
+		it "checks if d is equal to 3" do
+			expect(rsa.d).to eq 3 
+		end
+	end
+	describe "when trying to encrypt/decrypt a message "
 		rsa = RSA.new(1,1,1)
 		context "by using a random key, code encrypts and decrypts " do
 			it "a single character" do
@@ -27,7 +27,7 @@ RSpec.describe RSA do
 			it "a sentance" do
 				rsa.new_key
 				expect(rsa.decrypt(rsa.encrypt("Hi, how are you?"))).to eq "Hi, how are you?"
-      end
-    end
-  end
+      		end
+    	end
+  	end
 end
